3b6e0f41c23fb21666fa654a10d4b62b8312f18b
Mohd Omama <mohd.omama@gmail.com>
2018-06-11 09:48:48 +0530

    Thyme Leaf enabled

---
+++ src/main/java/com/snowden/giflib/controller/GifController.java
@@ -0,0 +1,20 @@
+package com.snowden.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+
+    @RequestMapping("/")
+    public String listGifs() {
+        return "home";
+    }
+
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String gifHome() {
+        return "The explorer for Gifs";
+    }
+}
